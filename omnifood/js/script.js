e4802365e749dc332ef154b6a21251eb9fb7a5c6
Arkadian404 <74943984+Arkadian404@users.noreply.github.com>
2022-07-13 12:44:12 +0700

    Add files via upload

---
+++ omnifood/js/script.js
@@ -0,0 +1,63 @@
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function (){
+    headerEl.classList.toggle("nav-open");
+});
+
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function(link){
+    link.addEventListener("click", function(e){
+        e.preventDefault();
+        const href = link.getAttribute('href');
+        if(href === "#")
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        
+        if(href!=="#" && href.startsWith("#")){
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({behavior:"smooth"});
+        }
+
+        if(link.classList.contains("main-nav-link")){
+            headerEl.classList.toggle("nav-open");
+        }
+    });
+});
+
+const sectionHeroEl = document.querySelector(".section-hero")
+const obs= new IntersectionObserver(function(entries){
+    const ent = entries[0];
+    if(!ent.isIntersecting)
+        document.body.classList.add("sticky");
+    if(ent.isIntersecting)
+        document.body.classList.remove("sticky");
+}, {
+    root: null,
+    threshold: 0,
+    //appear sticky when hit the margin soon 80px because sticky height is 80px
+    rootMargin: "-80px"
+});
+obs.observe(sectionHeroEl);
+
+function checkFlexGap(){
+    var flex =  document.createElement('div');
+    flex.style.display = "flex";
+    flex.style.flexDirection = "column";
+    flex.style.rowGap = "1px";
+
+    flex.appendChild(document.createElement("div"));
+    flex.appendChild(document.createElement("div"));
+
+    document.body.appendChild(flex);
+    var isSupported = flex.scrollHeight === 1;
+    flex.parentNode.removeChild(flex);
+
+    if(!isSupported)
+        document.body.classList.add("no-flexbox-gap");
+}
+
+checkFlexGap();
